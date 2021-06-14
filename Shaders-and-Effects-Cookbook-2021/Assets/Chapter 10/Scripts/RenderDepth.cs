e48afeaf919c13236ad86429e7116d3af4a39c00
John Doran <john@johnpdoran.com>
2021-06-14 00:02:21 -0500

    Added Chapter 10

---
+++ Shaders-and-Effects-Cookbook-2021/Assets/Chapter 10/Scripts/RenderDepth.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenderDepth : MonoBehaviour
+{
+    #region Variables
+    public Shader curShader;
+    public float depthPower = 0.2f;
+    private Material screenMat;
+    #endregion
+
+    #region Properties
+    Material ScreenMat
+    {
+        get
+        {
+            if (screenMat == null)
+            {
+                screenMat = new Material(curShader);
+                screenMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return screenMat;
+        }
+    }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!curShader && !curShader.isSupported)
+        {
+            enabled = false;
+        }
+    }
+
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (curShader != null)
+        {
+            ScreenMat.SetFloat("_DepthPower", depthPower);
+
+            Graphics.Blit(sourceTexture, destTexture, ScreenMat);
+        }
+        else
+        {
+            Graphics.Blit(sourceTexture, destTexture);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+        depthPower = Mathf.Clamp(depthPower, 0, 1);
+    }
+
+
+    void OnDisable()
+    {
+        if (screenMat)
+        {
+            DestroyImmediate(screenMat);
+        }
+    }
+
+}
